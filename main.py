0f92307ae902e7d059176320f332c4768539c20c
lord-carlos <155855+lord-carlos@users.noreply.github.com>
2022-01-15 13:57:44 +0100

    added config parser

--- main.py
+++ main.py
@@ -3,8 +3,21 @@
 import configparser
 
 def main():
+
+    print("Init config parser to read file")
     config = configparser.ConfigParser()
     config.read('config.ini')
+
+    print("Read ini file into variables")
+    token = config['telegram']['token']
+    url = config['owncloud']['url']
+    username = config['owncloud']['username']
+    password = config['owncloud']['password']
+    remote_file = config['owncloud']['remote_file']
+
+    print("Read all, starting bot.")
+    # todo: start bot here.
+
 
 if __name__ == "__main__":
     main()
